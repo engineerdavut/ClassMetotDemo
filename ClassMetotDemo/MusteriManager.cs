a7ec4d3ab894eef8a633fda259d814b929cdbff5
Davut Aktaş <engineerdavut@gmail.com>
2021-03-06 18:40:58 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle()
+        {
+            Console.WriteLine("Musteri Eklendi");
+        }
+    }
+}
